2cd5e3d268b2157d3e6b81aaaf5368d75ab7c147
Njoku Okechukwu Val <bonsoirval@gmail.com>
2019-02-19 13:58:20 +0100

    login and logout working

--- pyoffice/urls.py
+++ pyoffice/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.views.generic.base import TemplateView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    url(r'^login/$', auth_views.login, name = 'login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
+    #url(r'^logout/$', auth_views.logout, name = 'logout'),
     url(r'^admin/', admin.site.urls),
     url('qa', include('qa.urls')),
+    url(r'^$', TemplateView.as_view(template_name='site_page/index.html'),name='home'),
 ]
